f4064d5b4ba42bceea59c9543ebe4ba7057e1209
Raúl López Penalva <raullopezpenalva@icloud.com>
2026-03-04 15:54:43 +0100

    feat: enhance security configuration with additional filter chains

--- src/main/java/com/raullopezpenalva/contact_service/modules/contact/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/raullopezpenalva/contact_service/modules/contact/infrastructure/config/SecurityConfig.java
@@ -2,15 +2,19 @@ package com.raullopezpenalva.contact_service.modules.contact.infrastructure.conf
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
@@ -20,6 +24,17 @@ public class SecurityConfig {
                 .anyRequest().permitAll()
             )
             .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain publicChain(HttpSecurity http) throws Exception {
+        http
+            .securityMatcher("/**")
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
         return http.build();
     }
 }
